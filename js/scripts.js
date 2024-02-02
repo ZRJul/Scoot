f45599f83eae1b364f0507e21f3975f960bdc36d
ZRJul <mzr.juli@gmail.com>
2024-02-02 21:17:14 +0200

    js-1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 
 // connection header.html to all pages
-fetch('html/components/header.html')
-    .then(response => response.text())
-    .then(html => document.getElementById('header').innerHTML = html);
+// fetch('html/components/header.html')
+//     .then(response => response.text())
+//     .then(html => document.getElementById('header').innerHTML = html);
+
+
+    // Функция для асинхронной загрузки и вставки HTML-кода компонента
+    function loadComponent(componentUrl, containerId) {
+    fetch(componentUrl)
+        .then(response => response.text())
+        .then(html => document.getElementById(containerId).innerHTML = html);
+}
+
+    // Загрузка и вставка компонента header.html в контейнер headerContainer
+    loadComponent('html/components/header.html', 'header');
 
