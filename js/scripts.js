8b2236fe23d2691f4a41990c3bf23f88d5b1d4a1
ZRJul <mzr.juli@gmail.com>
2024-02-12 21:33:06 +0200

    home

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
-
 // connection header.html to all pages
 // fetch('html/components/header.html')
 //     .then(response => response.text())
 //     .then(html => document.getElementById('header').innerHTML = html);
 
 
-    // Функция для асинхронной загрузки и вставки HTML-кода компонента
-    function loadComponent(componentUrl, containerId) {
+// Функция для асинхронной загрузки и вставки HTML-кода компонента
+function loadComponent(componentUrl, containerId) {
     fetch(componentUrl)
         .then(response => response.text())
         .then(html => document.getElementById(containerId).innerHTML = html);
 }
 
-    // Загрузка и вставка компонента header.html в контейнер headerContainer
-    loadComponent('html/components/header.html', 'header');
+// Загрузка и вставка компонента header.html в контейнер headerContainer
+loadComponent('html/components/header.html', 'header');
 
+
+
+function loadComponentFooter(componentUrl, containerId) {
+    fetch(componentUrl)
+        .then(response => response.text())
+        .then(html => document.getElementById(containerId).innerHTML = html);
+}
+
+// Загрузка и вставка компонента footer.html в контейнер headerContainer
+loadComponentFooter('html/components/footer.html', 'footer');
